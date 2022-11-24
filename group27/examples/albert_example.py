9b5a68471241aff872f73a8b443efe3abc8c2bd9
Caspar Meijer <public.caspar@gmail.com>
2022-11-24 15:30:07 +0100

    Added examples and made main py file from which the project will be initialized.

---
+++ group27/examples/albert_example.py
@@ -0,0 +1,37 @@
+import gym
+import numpy as np
+from urdfenvs.robots.albert import AlbertRobot
+import warnings
+
+
+def run_albert(n_steps=1000, render=False, goal=True, obstacles=True):
+    robots = [
+        AlbertRobot(mode="vel"),
+    ]
+    env = gym.make(
+        "urdf-env-v0",
+        dt=0.01, robots=robots, render=render
+    )
+    action = np.zeros(9)
+    action[0] = 0.2
+    action[1] = 0.0
+    action[5] = -0.1
+    ob = env.reset(
+        pos=np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.5, 0.0, 1.8, 0.5])
+    )
+    print(f"Initial observation : {ob}")
+    history = []
+    for _ in range(n_steps):
+        ob, _, _, _ = env.step(action)
+        history.append(ob)
+    env.close()
+    return history
+
+
+if __name__ == "__main__":
+    show_warnings = False
+    warning_flag = "default" if show_warnings else "ignore"
+    with warnings.catch_warnings():
+        warnings.filterwarnings(warning_flag)
+        run_albert(render=True)
+
