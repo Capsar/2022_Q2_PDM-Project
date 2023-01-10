83d10e3e79f176d8a74f7f482e57ec915be76542
Marijn de Rijk <m.f.derijk@student.tudelt.nl>
2023-01-10 14:21:35 +0100

    added functions to transfrom between world and robot frame

--- group27/transforms.py
+++ group27/transforms.py
@@ -11,8 +11,18 @@ def get_T_world_robot(robot_config):
                                      [0, 0, 1]])
     translation = base_pos + base_rotation_matrix @ arm_mount_base_link_frame
 
-    T_matrix = np.hstack((base_rotation_matrix, translation[:,np.newaxis]))
+    T_matrix = np.hstack((base_rotation_matrix, -translation[:,np.newaxis]))
     T_matrix = np.vstack((T_matrix, np.array([0,0,0,1])))
     return T_matrix
+
+def T_robot_world(xyz_world, robot_config):
+   xyz_world_homogeneous = np.hstack((xyz_world, np.array([1]))) 
+   xyz_robot_homogeneous = get_T_world_robot(robot_config) @ xyz_world_homogeneous
+   return xyz_robot_homogeneous[:3]
+
+def T_world_robot(xyz_robot, robot_config):
+    xyz_robot_homogeneous = np.hstack((xyz_robot, np.array([1]))) 
+    xyz_world_homogeneous = np.linalg.inv(get_T_world_robot(robot_config)) @ xyz_robot_homogeneous
+    return xyz_world_homogeneous[:3]
 
 
