523a4ed5f84d6e3a956acfa9dc23bc21baa066d5
Marijn de Rijk <m.f.derijk@student.tudelt.nl>
2023-01-10 15:57:45 +0100

    added rotation matrices

--- group27/transforms.py
+++ group27/transforms.py
@@ -15,14 +15,27 @@ def get_T_world_robot(robot_config):
     T_matrix = np.vstack((T_matrix, np.array([0,0,0,1])))
     return T_matrix
 
-def T_robot_world(xyz_world, robot_config):
+def T_robot_world(xyz_world, robot_config): # from world to robot
    xyz_world_homogeneous = np.hstack((xyz_world, np.array([1]))) 
    xyz_robot_homogeneous = get_T_world_robot(robot_config) @ xyz_world_homogeneous
    return xyz_robot_homogeneous[:3]
 
-def T_world_robot(xyz_robot, robot_config):
+def T_world_robot(xyz_robot, robot_config): # from robot to world
     xyz_robot_homogeneous = np.hstack((xyz_robot, np.array([1]))) 
     xyz_world_homogeneous = np.linalg.inv(get_T_world_robot(robot_config)) @ xyz_robot_homogeneous
     return xyz_world_homogeneous[:3]
 
+def rotate_X(angle):
+    return np.array([[1, 0, 0],
+                     [0, np.cos(angle), -np.sin(angle)],
+                     [0, np.sin(angle), np.cos(angle)]])
 
+def rotate_Y(angle):
+    return np.array([[np.cos(angle), 0, np.sin(angle)],
+                     [0, 1, 0],
+                     [-np.sin(angle), 0, np.cos(angle)]])
+
+def rotate_Z(angle):
+    return np.array([[np.cos(angle), -np.sin(angle), 0],
+                     [np.sin(angle), np.cos(angle), 0],
+                     [0, 0, 1]])
