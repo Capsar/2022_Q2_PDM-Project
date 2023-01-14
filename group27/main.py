659cefd81e06e76131448ecd0635c96799df9403
Marijn de Rijk <m.f.derijk@student.tudelt.nl>
2023-01-14 15:04:59 +0100

    added command line arguments.  --environment sets the environmnet, and --arm_only=True sets the simulation to only consider the arm movements

--- group27/main.py
+++ group27/main.py
@@ -1,6 +1,8 @@
 import random
 import time
 import warnings
+import argparse
+from copy import deepcopy
 
 import gym
 import networkx as nx
@@ -69,7 +71,13 @@ def run_albert(n_steps=500000, render=True, goal=True, obstacles=True, at_end=Fa
         # Set the camera to be in top view of the environment
         p.resetDebugVisualizerCamera(cameraDistance=12, cameraYaw=0, cameraPitch=-89.99, cameraTargetPosition=[0, 0, 0])
 
-    # Perform no actions for couple of steps to spawn in the robot correctly (it starts in the air)
+        p.resetDebugVisualizerCamera(cameraDistance=16, cameraYaw=0, cameraPitch=-89.99, cameraTargetPosition=[0, 0, 0])
+
+
+    # Perform 1 random action to get the initial observation (containing obstacles & goal)
+    for step in range(100):
+        action = np.zeros(9)
+        ob, _, _, _ = env.step(action)
     action = np.zeros(env.n())
     for step in range(30):
         ob, _, _, _ = env.step(action)
@@ -118,7 +126,10 @@ def run_albert(n_steps=500000, render=True, goal=True, obstacles=True, at_end=Fa
                 action = base.pid_follow_path(ob)
                 if action == "DONE":
                     break
-            ob, _, _, _ = env.step(action)
+            ob, _, done, _ = env.step(action)
+            history.append(ob)
+            if done:
+                print("DONE")
 
     # Transform the camera to be close to the robot.
     transform_to_arm(ob)
@@ -192,4 +203,4 @@ if __name__ == "__main__":
     warning_flag = "default" if show_warnings else "ignore"
     with warnings.catch_warnings():
         warnings.filterwarnings(warning_flag)
-        run_albert()
+        run_albert(at_end=arm_only)
